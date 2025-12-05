f46fa07be0530c4510541115b4a8f3ac2b52fdfd
bikeshghising <tmgbikesh@gmail.com>
2025-12-05 15:06:28 -0500

    current weather API URL builder

--- javaScript.js
+++ javaScript.js
@@ -2,10 +2,13 @@
 async function getWeather() {
     const city = document.getElementById("cityInput").value;
     const apiKey = "";
+    // If no city name entered, show a message
+    if (city === "") {
+        document.getElementById("result").innerHTML = "<p>Please enter a city.</p>";
+        return;
+    }
+     // URL for current weather
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
 
-// If no city name entered, show a message
-if (city === "") {
-    document.getElementById("result").innerHTML = "<p>Please enter a city.</p>";
-    return;
- }
+
 }
