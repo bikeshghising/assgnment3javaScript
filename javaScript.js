418cd25f338ed23673e6d07e35cf35de38a8a96f
bikeshghising <tmgbikesh@gmail.com>
2025-12-05 15:12:03 -0500

    API fetch and error handling for getWeather

--- javaScript.js
+++ javaScript.js
@@ -9,6 +9,14 @@ async function getWeather() {
     }
      // URL for current weather
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
-
-
+    try {
+    // Fetch API data
+    const response = await fetch(url);
+    const data = await response.json();
+    
+    // If API returns an error code
+    if (data.cod !== 200) {
+        document.getElementById("result").innerHTML = `<p>Error: ${data.message}</p>`;
+        return;
+    }
 }
