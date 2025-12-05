815f346134e8134c0516ccb14322be75e85a14d9
bikeshghising <tmgbikesh@gmail.com>
2025-12-05 15:03:33 -0500

    empty city input validation

--- javaScript.js
+++ javaScript.js
@@ -2,4 +2,10 @@
 async function getWeather() {
     const city = document.getElementById("cityInput").value;
     const apiKey = "";
+
+// If no city name entered, show a message
+if (city === "") {
+    document.getElementById("result").innerHTML = "<p>Please enter a city.</p>";
+    return;
+ }
 }
